b8b4af71016c42b3f58c141a2972b6243e8d7c15
AngelPlaiesu <angelplaiesu@gmail.com>
2024-02-29 23:19:34 +0100

    Tags Front-end

--- resources/js/Components/Posts/FormCard.vue
+++ resources/js/Components/Posts/FormCard.vue
@@ -1,12 +1,10 @@
 <script setup>
-import TextInput from '@/Components/TextInput.vue';
 import draggable from 'vuedraggable'
-
 import Modal from "@/Components/Modal.vue";
 import ContentTextarea from "@/Components/Posts/Modals/ContentTextarea.vue";
 import ContentMedia from "@/Components/Posts/Modals/ContentMedia.vue";
 import ContentSearch from "@/Components/Posts/Modals/ContentSearch.vue";
-
+import Vue3TagsInput from 'vue3-tags-input';
 </script>
 <template>
     <div
@@ -18,11 +16,14 @@ import ContentSearch from "@/Components/Posts/Modals/ContentSearch.vue";
 
             <div class="flex justify-center align-center items-center">
                 <textarea id="message" rows="4" v-model="PostForm.content"
-                          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                          placeholder="Write your thoughts here..."></textarea>
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Write your thoughts here..."></textarea>
                 <div class="absolute">
-                    <svg @click="modalBoolean('textarea')" class="w-8 h-8 fill-gray-500 dark:fill-gray-400 hover:fill-amber-300 hover:cursor-pointer" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M208.8584,144a15.85626,15.85626,0,0,1-10.46778,15.01367l-52.16015,19.2168-19.2168,52.16015a16.00075,16.00075,0,0,1-30.02734,0l-19.2168-52.16015-52.16015-19.2168a16.00075,16.00075,0,0,1,0-30.02734l52.16015-19.2168,19.2168-52.16015a16.00075,16.00075,0,0,1,30.02734,0l19.2168,52.16015,52.16015,19.2168A15.85626,15.85626,0,0,1,208.8584,144ZM152,48h16V64a8,8,0,0,0,16,0V48h16a8,8,0,0,0,0-16H184V16a8,8,0,0,0-16,0V32H152a8,8,0,0,0,0,16Zm88,32h-8V72a8,8,0,0,0-16,0v8h-8a8,8,0,0,0,0,16h8v8a8,8,0,0,0,16,0V96h8a8,8,0,0,0,0-16Z"/>
+                    <svg @click="modalBoolean('textarea')"
+                        class="w-8 h-8 fill-gray-500 dark:fill-gray-400 hover:fill-amber-300 hover:cursor-pointer"
+                        viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M208.8584,144a15.85626,15.85626,0,0,1-10.46778,15.01367l-52.16015,19.2168-19.2168,52.16015a16.00075,16.00075,0,0,1-30.02734,0l-19.2168-52.16015-52.16015-19.2168a16.00075,16.00075,0,0,1,0-30.02734l52.16015-19.2168,19.2168-52.16015a16.00075,16.00075,0,0,1,30.02734,0l19.2168,52.16015,52.16015,19.2168A15.85626,15.85626,0,0,1,208.8584,144ZM152,48h16V64a8,8,0,0,0,16,0V48h16a8,8,0,0,0,0-16H184V16a8,8,0,0,0-16,0V32H152a8,8,0,0,0,0,16Zm88,32h-8V72a8,8,0,0,0-16,0v8h-8a8,8,0,0,0,0,16h8v8a8,8,0,0,0,16,0V96h8a8,8,0,0,0,0-16Z" />
                     </svg>
                 </div>
             </div>
@@ -65,11 +66,18 @@ import ContentSearch from "@/Components/Posts/Modals/ContentSearch.vue";
             </div>
 
             <div class="flex justify-between w-full ">
-                <svg @click="modalBoolean('search')" class="w-8 h-8 stroke-gray-500 dark:stroke-gray-500 hover:fill-amber-300 hover:cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <svg @click="modalBoolean('search')"
+                    class="w-8 h-8 stroke-gray-500 dark:stroke-gray-500 hover:fill-amber-300 hover:cursor-pointer"
+                    viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
-                <svg @click="modalBoolean('media')" class="w-8 h-8 fill-gray-500 dark:fill-gray-400 hover:fill-amber-300 hover:cursor-pointer" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M208.8584,144a15.85626,15.85626,0,0,1-10.46778,15.01367l-52.16015,19.2168-19.2168,52.16015a16.00075,16.00075,0,0,1-30.02734,0l-19.2168-52.16015-52.16015-19.2168a16.00075,16.00075,0,0,1,0-30.02734l52.16015-19.2168,19.2168-52.16015a16.00075,16.00075,0,0,1,30.02734,0l19.2168,52.16015,52.16015,19.2168A15.85626,15.85626,0,0,1,208.8584,144ZM152,48h16V64a8,8,0,0,0,16,0V48h16a8,8,0,0,0,0-16H184V16a8,8,0,0,0-16,0V32H152a8,8,0,0,0,0,16Zm88,32h-8V72a8,8,0,0,0-16,0v8h-8a8,8,0,0,0,0,16h8v8a8,8,0,0,0,16,0V96h8a8,8,0,0,0,0-16Z"/>
+                <svg @click="modalBoolean('media')"
+                    class="w-8 h-8 fill-gray-500 dark:fill-gray-400 hover:fill-amber-300 hover:cursor-pointer"
+                    viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M208.8584,144a15.85626,15.85626,0,0,1-10.46778,15.01367l-52.16015,19.2168-19.2168,52.16015a16.00075,16.00075,0,0,1-30.02734,0l-19.2168-52.16015-52.16015-19.2168a16.00075,16.00075,0,0,1,0-30.02734l52.16015-19.2168,19.2168-52.16015a16.00075,16.00075,0,0,1,30.02734,0l19.2168,52.16015,52.16015,19.2168A15.85626,15.85626,0,0,1,208.8584,144ZM152,48h16V64a8,8,0,0,0,16,0V48h16a8,8,0,0,0,0-16H184V16a8,8,0,0,0-16,0V32H152a8,8,0,0,0,0,16Zm88,32h-8V72a8,8,0,0,0-16,0v8h-8a8,8,0,0,0,0,16h8v8a8,8,0,0,0,16,0V96h8a8,8,0,0,0,0-16Z" />
                 </svg>
             </div>
 
@@ -84,6 +92,11 @@ import ContentSearch from "@/Components/Posts/Modals/ContentSearch.vue";
                     </div>
                 </label>
             </div>
+
+            <Vue3TagsInput :tags="tags"
+                class="flex flex-col sm:flex-row items-center justify-between w-full p-4 text-gray-900 border border-gray-300 rounded-lg  text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter post tags" @on-tags-changed="handleChangeTag" />
+
             <div class="flex gap-5 justify-between content-stretch flex-col lg:flex-row ">
                 <VDatePicker v-model="PostForm.postDate" mode="dateTime">
                     <template #default="{ inputValue, inputEvents }">
@@ -95,7 +108,7 @@ import ContentSearch from "@/Components/Posts/Modals/ContentSearch.vue";
                     <button type="button"
                         class=" flex-1  h-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Publish</button>
                     <button type="submit"
-                        class=" flex-1  h-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" >Save</button>
+                        class=" flex-1  h-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
                 </span>
             </div>
         </form>
@@ -108,10 +121,12 @@ import ContentSearch from "@/Components/Posts/Modals/ContentSearch.vue";
             <ContentSearch v-if="type == 'search'" />
 
             <div class="flex justify-between items-center gap-4 mt-4">
-                <button type="button" class=" flex-1 h-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                <button type="button"
+                    class=" flex-1 h-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                     Save
                 </button>
-                <button @click="modalBoolean" type="button" class=" flex-1 h-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                <button @click="modalBoolean" type="button"
+                    class=" flex-1 h-100 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                     Close
                 </button>
             </div>
@@ -127,6 +142,8 @@ export default {
         return {
             boolean: false,
             type: null,
+            tag: '',
+            tags: [],
         };
     },
 
@@ -134,9 +151,9 @@ export default {
         handleFileUpload(event) {
             for (let i = 0; i < event.target.files.length; i++) {
                 let file = {
-                        file: event.target.files[i],
-                        url: URL.createObjectURL(event.target.files[i])
-                    };
+                    file: event.target.files[i],
+                    url: URL.createObjectURL(event.target.files[i])
+                };
                 this.PostForm.orderedFiles.push(file);
             }
         },
@@ -167,12 +184,33 @@ export default {
         modalBoolean(type) {
             this.boolean = !this.boolean
             this.type = type
-        }
-    },   
+        },
+        handleChangeTag(tags) {
+            this.tags = tags;
+        },
+
+
+    },
     components: {
         draggable,
+        Vue3TagsInput
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css">
+    .v3ti .v3ti-tag {
+        background: #374151;
+        border: 1px solid #222222;
+        border-radius: 0;
+    }
+
+    .v3ti .v3ti-tag .v3ti-remove-tag {
+        color: #000000;
+        transition: color .3s;
+    }
+
+    .v3ti .v3ti-tag .v3ti-remove-tag:hover {
+        color: #ffffff;
+    }
+</style>
